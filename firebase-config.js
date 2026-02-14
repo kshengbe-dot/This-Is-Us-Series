4df1e70aee825ce3e3512b38b21ffc0eee785650
kshengbe-dot <kshengbe@gmail.com>
2026-02-13 20:27:13 -0700

    Add Firebase configuration settings

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,10 @@
-
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyAMgeJrpAaJNA4CIwAtHUZuicDVN45wuFI",
+  authDomain: "this-is-us-series.firebaseapp.com",
+  projectId: "this-is-us-series",
+  storageBucket: "this-is-us-series.firebasestorage.app",
+  messagingSenderId: "7858773486",
+  appId: "1:7858773486:web:0fb6899916f0924ec482ff",
+  measurementId: "G-3VX90NHJFG"
+};
