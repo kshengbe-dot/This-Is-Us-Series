58d3da542b5738a1eb699581cf08794b4eae2367
kshengbe-dot <kshengbe@gmail.com>
2026-02-13 23:05:37 -0700

    Modify firebase configuration parameters
    Updated appId and measurementId in firebaseConfig.

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
-// firebase-config.js
 export const firebaseConfig = {
   apiKey: "AIzaSyAMgeJrpAaJNA4CIwAtHUZuicDVN45wuFI",
   authDomain: "this-is-us-series.firebaseapp.com",
   projectId: "this-is-us-series",
   storageBucket: "this-is-us-series.firebasestorage.app",
   messagingSenderId: "7858773486",
-  appId: "1:7858773486:web:0fb6899916f0924ec482ff",
-  measurementId: "G-3VX90NHJFG"
+  appId: "1:7858773486:web:e0057e020ae98befc482ff",
+  measurementId: "G-GC9WDBR40W"
 };
